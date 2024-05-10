84cf2d384441a9245078f9b65e283baf128d1e2c
Thais <esx.tabe@terceiros.mag.com.br>
2024-05-10 14:28:20 -0300

    fazendo controller cadastro

--- src/main/java/com/portal_tech/portal_tech/controllers/controllersMVC/AutenticacaoController.java
+++ src/main/java/com/portal_tech/portal_tech/controllers/controllersMVC/AutenticacaoController.java
@@ -1,6 +1,8 @@
 package com.portal_tech.portal_tech.controllers.controllersMVC;
 
 import com.portal_tech.portal_tech.models.Pessoa;
+import com.portal_tech.portal_tech.models.Setor;
+import com.portal_tech.portal_tech.models.Tipo;
 import com.portal_tech.portal_tech.repositores.PessoaRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,6 +13,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class AutenticacaoController {
@@ -28,24 +33,15 @@ public class AutenticacaoController {
 
         Pessoa pessoa = this.pessoaRepository.verifyLogin(pessoaParam.getEmail(), pessoaParam.getSenha());//inf vindas do banco
         if(pessoa != null){ //o usuário esta cadastrado no banco
-
-        }
-
-        if(pessoa.getTipo().getNome().equals("Usuário")){
-            return "redirect:/index.usuario";
-        }else if(pessoa.getTipo().getNome().equals("Técnico")){
-            return "redirect:/index.usuario";
-        } else if (pessoa.getTipo().getNome().equals("Administrador")) {
-            return "redirect:/usuario/admin";
-        }else {
+        return "redirect:/index/usuario";
+        }else{
             model.addAttribute("erro", "Usuário ou senhas inválidos");//mensagem de erro na tela de login
             return "/login";
         }
     }
 
-    @RequestMapping(value = "/cadastro", method = RequestMethod.POST)
-    public String cadastrar(@RequestParam("senha") String senha){
-        System.out.println("senha   " + senha);
+    @RequestMapping(value = "/cadastro", method = RequestMethod.GET)
+    public String cadastrar(){
         return "/cadastro";
     }
 
@@ -53,24 +49,34 @@ public class AutenticacaoController {
 //    public String cadastro() {
 //        return "cadastro";
 //    }
-    @RequestMapping(value = "/cadastro", method = RequestMethod.GET)
-    public String cadastrar() {
 
-//        System.out.println("nome: " + nome + "telefone: " + telefone + "email: " + email + "senha " + senha);
-//       salvar essas informações no banco e depos retornar. Caso a pessoa for usuaria,,
-//       List<String> radioOptions = Arrays.asList("Usuário", "Técnico", "Administrador");
-//       model.addAttribute("raadioOptions", radioOptions);
-//        String personSelected = request.getParameter("tipo");
-//        String radio1 = radio;// recebendo a opção que a pessoa selecionou
-//        String setorSelected = request.getParameter("setor");
-//        String nome = request.getParameter("nome");
-//        String telefone = request.getParameter("telefone");
-//        String email = request.getParameter("email");
-//        String senha = request.getParameter("senha");
-//        System.out.println(" A opção selecionada do radio foi: " + personSelected + "A opção do select setor com RequestParam : " + radio1 + " O setor: " + setorSelected
-//                + " nome: " + nome + "telefone " + telefone + "email: " + email + "senha: " + senha);
-//        System.out.println("personSelected: " + personSelected + "radio " + radio1);
-        return "/cadastro";
+//    public String cadastrar(@RequestParam("nome") String nome,@RequestParam("telefone") String telefone, @RequestParam("email") String email,@RequestParam("senha") String senha, Model model, HttpServletRequest request) {
+    @RequestMapping(value = "/cadastro", method = RequestMethod.POST)
+    public String cadastrar(Model model, HttpServletRequest request){
+
+        String setorSelected = request.getParameter("optionSetor");
+        String tipoSelected = request.getParameter("optionTipo");
+        String nome = request.getParameter("nome");
+        String telefone = request.getParameter("telefone");
+        String email = request.getParameter("email");
+        String senha = request.getParameter("senha");
+        System.out.println(" A opção selecionada do radio foi: " + "personSelected" + "  O setor: " + setorSelected
+                + " nome1: " + nome + "telefone " + telefone + "email: " + email + "senha: " + senha);
+        System.out.println("personSelected: " );
+
+        Pessoa pessoa = new Pessoa();
+        Tipo tipo = new Tipo();
+        tipo.setNome("Usuario");
+
+        Setor setor = new Setor();
+        setor.setNome(setorSelected);
+
+        pessoa.setNome(nome);
+        pessoa.setTelefone(telefone);
+        pessoa.setEmail(email);
+        pessoa.setSenha(senha);
+        this.pessoaRepository.save(pessoa);
+        return "/login";
 
     }
 }
