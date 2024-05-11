5bcc66851cd83501aefe355b34f445834f5aca17
Thais <esx.tabe@terceiros.mag.com.br>
2024-05-11 12:10:14 -0300

    add method getPessoa() on controller

--- src/main/java/com/portal_tech/portal_tech/services/AutenticacaoService.java
+++ src/main/java/com/portal_tech/portal_tech/services/AutenticacaoService.java
@@ -4,6 +4,7 @@ import com.portal_tech.portal_tech.models.Pessoa;
 import com.portal_tech.portal_tech.models.Setor;
 import com.portal_tech.portal_tech.models.Tipo;
 import com.portal_tech.portal_tech.repositores.PessoaRepository;
+import com.portal_tech.portal_tech.repositores.SetorRepository;
 import com.portal_tech.portal_tech.repositores.TipoRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,25 +24,29 @@ public class AutenticacaoService {
     @Autowired
     private TipoRepository tipoRepository;
 
+    @Autowired
+    private SetorRepository setorRepository;
 
-    public String cadastrar(Model model, HttpServletRequest request, @RequestParam("nome") String nome, @RequestParam("telefone") String telefone, @RequestParam("email") String email, @RequestParam("senha") String senha) {
+    public String cadastrar(Model model, HttpServletRequest request,  String nome,
+                             String telefone, String email,String senha) {
 
-        List<Tipo> listOfTypes = new ArrayList<>();
-        listOfTypes = this.tipoRepository.findAll();
+        List<Tipo> listOfTypes = this.tipoRepository.findAll();
 
-        List<String> radioTipoOptions = Arrays.asList("Usuário", "Técnico", "Administrador");
+        List<Setor> listOfSetor = this.setorRepository.findAll();
+
         model.addAttribute("optionTipo", listOfTypes);
 
-        List<String> radioSetorOptions = Arrays.asList("Administrativo", "Marketing", "Recursos Humanos", "Suporte");
-        model.addAttribute("radioSetorOptions", radioSetorOptions);
-
+//        List<String> radioSetorOptions = Arrays.asList("Administrativo", "Marketing", "Recursos Humanos", "Suporte");
+        model.addAttribute("radioSetorOptions", listOfSetor );
 
         String tipoSelected = request.getParameter("tipo");
 
-        System.out.println(" A opção selecionada do radio foi: " + "personSelected" + "  O setor: "
-                + " nome1: " + nome + "telefone " + telefone + "email: " + email + "senha: " + senha);
-        System.out.println("personSelected: ");
+        Pessoa pessoa = getPessoa(nome, telefone, email, senha, tipoSelected);
+        this.pessoaRepository.save(pessoa);
+        return "/login";
+}
 
+    private static Pessoa getPessoa(String nome, String telefone, String email, String senha, String tipoSelected) {
         Pessoa pessoa = new Pessoa();
         Tipo tipo = new Tipo();
         tipo.setNome(tipoSelected);
@@ -54,7 +59,6 @@ public class AutenticacaoService {
         pessoa.setTelefone(telefone);
         pessoa.setEmail(email);
         pessoa.setSenha(senha);
-        this.pessoaRepository.save(pessoa);
-        return "/login";
-}
+        return pessoa;
+    }
 }
