200fbb3eca1b2a024ffc86c12c45850915a2d860
Thais <esx.tabe@terceiros.mag.com.br>
2024-05-12 23:15:46 -0300

    add documentarion on StatusController

--- src/main/java/com/portal_tech/portal_tech/controllers/controllersREST/StatusController.java
+++ src/main/java/com/portal_tech/portal_tech/controllers/controllersREST/StatusController.java
@@ -3,6 +3,8 @@ package com.portal_tech.portal_tech.controllers.controllersREST;
 import com.portal_tech.portal_tech.models.dtos.StatusDTO;
 import com.portal_tech.portal_tech.services.StatusService;
 import com.portal_tech.portal_tech.swaggerDoc.StatusControllerOpenApi;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,10 @@ public class StatusController implements StatusControllerOpenApi {
 
     @Autowired
     private StatusService statusService;
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "os dados foram salvos com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Faltam dados obrigatórios a serem passados/não foi possível salvar")
+    })
     @PostMapping("/save")
     public ResponseEntity<StatusDTO> save(@RequestBody StatusDTO statusDTO){
         return this.statusService.save(statusDTO);
@@ -24,17 +30,30 @@ public class StatusController implements StatusControllerOpenApi {
     public ResponseEntity<List<StatusDTO>> findAll(){
       return this.statusService.findAll();
     }
-
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "O ID requisitado foi encontrado"),
+            @ApiResponse(responseCode = "404", description = "O ID requisitado não existe/foi encontrado no sistema")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<StatusDTO> findById(@PathVariable Long id){
      return this.statusService.findById(id);
     }
 
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "O elemento foi deletado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "O ID requisitado não existe/foi encontrado no sistema")
+    })
     @DeleteMapping(value="delete/{id}")
     public ResponseEntity<String> deleteById(@PathVariable Long id){
         return  this.statusService.deleteById(id);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "as alterações foram realizadas com sucesso"),
+            @ApiResponse(responseCode = "204", description = "faltam dados a serem passados no body/url" ),
+            @ApiResponse(responseCode = "500", description = "O ID requisitado não existe/foi encontrado no sistema")
+    })
     @PutMapping("update/{id}")
     public ResponseEntity<StatusDTO> updateById(@PathVariable Long id, @RequestBody StatusDTO statusDTO){
         return this.statusService.updateById(id,statusDTO);
