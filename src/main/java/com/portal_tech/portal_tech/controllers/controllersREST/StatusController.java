53416fd16cf47357ffd8f5a03b9fdfdd7ce5be05
Thais <esx.tabe@terceiros.mag.com.br>
2024-05-12 23:13:21 -0300

    fix return methods on StatusControllers

--- src/main/java/com/portal_tech/portal_tech/controllers/controllersREST/StatusController.java
+++ src/main/java/com/portal_tech/portal_tech/controllers/controllersREST/StatusController.java
@@ -1,60 +1,43 @@
 package com.portal_tech.portal_tech.controllers.controllersREST;
 
-import com.portal_tech.portal_tech.models.Status;
-import com.portal_tech.portal_tech.repositores.StatusRepository;
+import com.portal_tech.portal_tech.models.dtos.StatusDTO;
+import com.portal_tech.portal_tech.services.StatusService;
 import com.portal_tech.portal_tech.swaggerDoc.StatusControllerOpenApi;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
-@RestController
 
+@RestController
+@RequestMapping("status")
 public class StatusController implements StatusControllerOpenApi {
 
     @Autowired
-    private StatusRepository statusRepository;
-
-    @RequestMapping(value="cadstatus", method = RequestMethod.POST)
-    public Status save(@RequestBody Status status){
-        return statusRepository.save(status);
+    private StatusService statusService;
+    @PostMapping("/save")
+    public ResponseEntity<StatusDTO> save(@RequestBody StatusDTO statusDTO){
+        return this.statusService.save(statusDTO);
     }
 
     @RequestMapping(value="/status", method = RequestMethod.GET)
-    public List<Status> findAll(){
-        List<Status> status = statusRepository.findAll();
-        if (status.isEmpty()){ //se estiver vazio, não encontrou nada cadastrado
-            throw new RuntimeException("Não há status cadastrados!");
-        }
-        else {
-            return status;
-        }
+    public ResponseEntity<List<StatusDTO>> findAll(){
+      return this.statusService.findAll();
     }
 
-    @RequestMapping(value="status/{id}", method = RequestMethod.GET)
-    public Status findById(@PathVariable Long id){
-        Optional<Status > resultado = this.statusRepository.findById(id);
-        if (resultado.isEmpty()){
-            throw new RuntimeException("Status não encontrado!");
-        }
-        else {
-            return resultado.get(); //usei get para retornar o objeto dentro de Optional
-        }
+    @GetMapping("/{id}")
+    public ResponseEntity<StatusDTO> findById(@PathVariable Long id){
+     return this.statusService.findById(id);
     }
 
-    @DeleteMapping(value="excstatus/{id}")
-    public Status deleteById(@PathVariable Long id){
-        Status status = findById(id);
-        this.statusRepository.deleteById(id);
-        return status;
+    @DeleteMapping(value="delete/{id}")
+    public ResponseEntity<String> deleteById(@PathVariable Long id){
+        return  this.statusService.deleteById(id);
     }
 
-    @RequestMapping(value = "edstatus/{id}", method = RequestMethod.PUT)
-    public Status updateById(@PathVariable Long id, @RequestBody Status status){
-        this.findById(id);
-        status.setId(id);
-        status = this.statusRepository.save(status);
-        return status;
+    @PutMapping("update/{id}")
+    public ResponseEntity<StatusDTO> updateById(@PathVariable Long id, @RequestBody StatusDTO statusDTO){
+        return this.statusService.updateById(id,statusDTO);
     }
 
 }
--- src/main/java/com/portal_tech/portal_tech/controllers/controllersREST/TipoContoller.java
+++ src/main/java/com/portal_tech/portal_tech/controllers/controllersREST/TipoContoller.java
@@ -4,6 +4,8 @@ package com.portal_tech.portal_tech.controllers.controllersREST;
 import com.portal_tech.portal_tech.models.dtos.TipoDTO;
 import com.portal_tech.portal_tech.services.TipoService;
 import com.portal_tech.portal_tech.swaggerDoc.TipoControllerOpenApi;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,27 +19,44 @@ public class TipoContoller implements TipoControllerOpenApi {
     @Autowired
     private TipoService tipoService;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "os dados foram salvos com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Faltam dados obrigatórios a serem passados/não foi possível salvar")
+    })
     @PostMapping("/save")
     public ResponseEntity<TipoDTO> save(@RequestBody TipoDTO tipoDTO){
 
         return this.tipoService.save(tipoDTO);
     }
-
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Erro interno")
+    })
     @GetMapping
     public ResponseEntity<List<TipoDTO>> findAll(){
             return this.tipoService.findAll();
     }
-
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "O ID requisitado foi encontrado"),
+            @ApiResponse(responseCode = "404", description = "O ID requisitado não existe/foi encontrado no sistema")
+    })
     @RequestMapping(value="/{id}", method = RequestMethod.GET)
     public ResponseEntity<TipoDTO> findById(@PathVariable Long id){
        return this.tipoService.findById(id);
     }
-
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "O elemento foi deletado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "O ID requisitado não existe/foi encontrado no sistema")
+    })
     @DeleteMapping(value="/{id}")
     public ResponseEntity<String> deleteById(@PathVariable Long id){
         return this.tipoService.deleteById(id);
     }
-
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "as alterações foram realizadas com sucesso"),
+            @ApiResponse(responseCode = "204", description = "faltam dados a serem passados no body/url" ),
+            @ApiResponse(responseCode = "500", description = "O ID requisitado não existe/foi encontrado no sistema")
+    })
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResponseEntity<TipoDTO> updateById(@PathVariable Long id, @RequestBody TipoDTO tipoDTO){
         return this.tipoService.updateById(id, tipoDTO);
